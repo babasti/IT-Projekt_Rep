44577e5a7fd0be560f4b4588c0610d85288790c2
alenbrisevac <alenbrisevac@10.207.15.95>
2016-10-17 16:45:52 +0200

    MADAFAKAAAAA

---
+++ A-Team_IT-Projekt/src/client/MainTest.java
@@ -0,0 +1,33 @@
+package client;
+
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MainTest extends Application {
+	
+	
+
+	public static void main(String[] args) {
+		
+		launch();
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try{
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Test.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
