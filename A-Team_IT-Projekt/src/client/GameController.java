060ef78921b301a5f0ecd7e22f9f580274628823
simi <simi@10.207.15.188>
2016-10-17 15:37:12 +0200

    game

---
+++ A-Team_IT-Projekt/src/client/GameController.java
@@ -0,0 +1,32 @@
+package client;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class GameController implements Initializable {
+
+	@FXML
+	Button b1 = new Button ("1");
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void sendToServer(ActionEvent e){
+		
+		
+	
+	
+	}
+	
+}
+	
