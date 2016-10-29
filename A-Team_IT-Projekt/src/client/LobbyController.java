f7d38fb01d39ba6b6fdb8b1d26b6d1000cba3733
alenbrisevac <alenbrisevac@desktop-704tkv6.edu.ds.fhnw.ch>
2016-10-29 15:09:17 +0200

    GUI - Lobby

---
+++ A-Team_IT-Projekt/src/client/LobbyController.java
@@ -0,0 +1,40 @@
+package client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class LobbyController implements Initializable {
+	
+	
+	
+	
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void createNewSession(){
+		
+	}
+	
+	public void exitSession(){
+		
+	}
+	
+	public void joinSession(){
+		
+	}
+	
+	public void selectSession(){
+		
+	}
+	
+	public void startSession(){
+		
+	}
+
+}
