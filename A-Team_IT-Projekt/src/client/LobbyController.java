dc8f501df125c0f355c3cce2a972c8081d5d1471
alenbrisevac <alenbrisevac@10.207.3.124>
2016-11-05 12:32:02 +0100

    GUI - Lobby

--- A-Team_IT-Projekt/src/client/LobbyController.java
+++ A-Team_IT-Projekt/src/client/LobbyController.java
@@ -2,18 +2,67 @@ package client;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class LobbyController implements Initializable {
 	
+	ObservableList<Integer> cb_AnzahlSpielerList = (ObservableList<Integer>) FXCollections.observableArrayList(2,3,4);
 	
+	@FXML
+	Label text_OffeneSitzungen;
 	
+	@FXML
+	TableView table_OffeneSitzungen;
 	
+	@FXML
+	TableColumn spalte_Sitzung;
 	
-
+	@FXML
+	TableColumn spalte_Spieler;
+	
+	@FXML
+	TableColumn spalte_Anzahl;
+	
+	 @FXML
+	 Button b_SitzungBeitreten;
+	 
+	 @FXML
+	 Button b_SitzungVerlassen;
+	 
+	 @FXML
+	 Button b_SpielStarten;
+	 
+	 @FXML
+	 Label text_NeueSitzungErstellen;
+	 
+	 @FXML
+	 Label text_Sitzungsname;
+	 
+	 @FXML
+	 Label text_AnzahlSpieler;
+	 
+	 @FXML
+	 TextField tf_Sitzungsname;
+	 
+	 @FXML
+	 ChoiceBox cb_AnzahlSpieler;
+	 
+	 @FXML
+	 Button b_SitzungErstellen;
+	 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		cb_AnzahlSpieler.setItems(cb_AnzahlSpielerList);
 		
 	}
 	
