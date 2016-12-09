b0e42797121b849f66f6536c89fc532b52c694f4
Barbara <Barbara@10.207.3.192>
2016-12-09 17:16:53 +0100

    bo

--- A-Team_IT-Projekt/src/client/LobbyController.java
+++ A-Team_IT-Projekt/src/client/LobbyController.java
@@ -205,7 +205,9 @@ public class LobbyController implements Initializable {
 	}
 
 	public void startSession(){
-	
+		Session s = this.selectSession();
+        Game g = new Game(s, b_SpielStarten);
+        ClientThread.sendToServer(g);
 	}
 
 }
