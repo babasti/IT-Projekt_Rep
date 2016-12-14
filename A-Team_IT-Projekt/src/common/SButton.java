5067a2bf25244901d1879b99b65ca64d64295a8b
simi <simi@Simone>
2016-12-14 19:06:55 +0100

    SButton

---
+++ A-Team_IT-Projekt/src/common/SButton.java
@@ -0,0 +1,13 @@
+package common;
+
+import java.io.Serializable;
+
+import javafx.scene.control.Button;
+
+public class SButton extends Button implements Serializable {
+	
+	public SButton(){
+		super();
+	}
+
+}
